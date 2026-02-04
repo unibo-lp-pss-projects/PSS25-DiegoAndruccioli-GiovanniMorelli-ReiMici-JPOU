2b7c06441f3bef79b6ab4adc19125551b4e9288b
Rei Mici <rei.mici2@studio.unibo.it>
2026-02-04 09:50:48 +0100

    add skin interface

--- src/main/java/it/unibo/samplejavafx/mvcexample/model/items/durable/skin/Skin.java
+++ src/main/java/it/unibo/samplejavafx/mvcexample/model/items/durable/skin/Skin.java
@@ -1,4 +1,18 @@
 package it.unibo.samplejavafx.mvcexample.model.items.durable.skin;
 
-public interface Skin {
+import it.unibo.samplejavafx.mvcexample.model.items.durable.Durable;
+
+/**
+ * Represents a specific durable item that changes the visual appearance (color) of the character.
+ * Implementations of this interface (Red, Green, Default) will provide specific CSS color codes.
+ */
+public interface Skin extends Durable {
+
+    /**
+     * Returns the CSS hexadecimal color code associated with this skin.
+     * Used by the view to fill the character's body shape.
+     *
+     * @return a string representing the color.
+     */
+    String getColorHex();
 }
