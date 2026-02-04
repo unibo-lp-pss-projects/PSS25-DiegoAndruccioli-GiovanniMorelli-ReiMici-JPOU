1b111e3ec73729dce6a5bc6152a6df310f6edaca
Giovanni Morelli <giovanni.morelli8@studio.unibo.it>
2026-02-04 19:26:20 +0100

    implement fcg controller

---
+++ src/main/java/it/unibo/jpou/mvc/controller/minigames/FruitCatcherController.java
@@ -0,0 +1,8 @@
+package it.unibo.jpou.mvc.controller.minigames;
+
+/**
+ * Controller interface for the Fruit Catcher minigame.
+ * Handles the game loop and user input.
+ */
+public interface FruitCatcherController {
+}
