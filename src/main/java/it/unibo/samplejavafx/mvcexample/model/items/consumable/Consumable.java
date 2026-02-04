c7f64c78e3bdc4c078c89c63b304e33348f960fe
Rei Mici <rei.mici2@studio.unibo.it>
2026-02-04 09:30:59 +0100

    Consumable interface

---
+++ src/main/java/it/unibo/samplejavafx/mvcexample/model/items/consumable/Consumable.java
@@ -0,0 +1,20 @@
+package it.unibo.samplejavafx.mvcexample.model.items.consumable;
+
+import it.unibo.samplejavafx.mvcexample.model.items.Item;
+
+/**
+ * Represents a specific category of items that are removed from the inventory after the use.
+ * Unlike durable items, consumables provide a quantitative effect on the Pou's status and have a single-use lifecycle.
+ */
+public interface Consumable extends Item {
+
+    /**
+     * Returns the quantitative magnitude of the item's effect.
+     * The interpretation of this value depends on the concrete implementation:
+     * For {@code Food}, it represents the hunger reduction value.
+     * For {@code Potion}, it represents the health restoration value.
+     *
+     * @return the positive integer representing the effect power.
+     */
+    int getEffectValue();
+}
