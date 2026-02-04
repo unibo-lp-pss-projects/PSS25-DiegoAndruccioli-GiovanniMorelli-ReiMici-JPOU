cfdb9dfa4169cabebd1f8ef3fc21ba057d2d549e
Giovanni Morelli <giovanni.morelli8@studio.unibo.it>
2026-02-04 19:22:34 +0100

    add fcg render view

--- src/main/java/it/unibo/jpou/mvc/view/minigames/FruitCatcherView.java
+++ src/main/java/it/unibo/jpou/mvc/view/minigames/FruitCatcherView.java
@@ -2,3 +2,19 @@ package it.unibo.jpou.mvc.view.minigames;
 
 import it.unibo.jpou.mvc.model.minigames.fruitcatcher.FallingObject;
 import java.util.List;
+
+/**
+ * Interface for the visual representation of the Fruit Catcher minigame.
+ */
+@FunctionalInterface //necessario per interfacce con un solo metood astratto
+public interface FruitCatcherView {
+
+    /**
+     * Renders the current state of the game.
+     *
+     * @param objects  the list of falling objects to draw.
+     * @param score    the current score.
+     * @param gameOver true if the game is over (to show end screen), false otherwise.
+     */
+    void render(List<FallingObject> objects, int score, boolean gameOver);
+}
