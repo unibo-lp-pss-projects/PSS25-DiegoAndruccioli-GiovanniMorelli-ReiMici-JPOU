8a58640c105b1a043f75059ec288e43518ed47fa
Rei Mici <rei.mici2@studio.unibo.it>
2026-02-05 12:25:16 +0100

    Fix magic numbers in GameRoomLogicTest

--- src/test/java/it/unibo/jpou/mvc/model/roomlogic/GameRoomLogicTest.java
+++ src/test/java/it/unibo/jpou/mvc/model/roomlogic/GameRoomLogicTest.java
@@ -1,6 +1,5 @@
 package it.unibo.jpou.mvc.model.roomlogic;
 
-import it.unibo.jpou.mvc.model.PouStatistics;
 import it.unibo.jpou.mvc.model.statistics.FunStatistic;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -9,34 +8,21 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class GameRoomLogicTest {
 
-    private static final int SET_FUN_VALUE = 99;
-
-    private PouStatistics fun;
+    private static final int INITIAL_VALUE = 50;
     private GameRoomLogic gameRoomLogic;
+    private FunStatistic fun;
 
     @BeforeEach
     void setUp() {
-        this.fun = new FunStatistic();
-        this.gameRoomLogic = new GameRoomLogic();
+        gameRoomLogic = new GameRoomLogic();
+        fun = new FunStatistic();
+        fun.setValueStat(INITIAL_VALUE);
     }
 
     @Test
-    void testPlayIncreasesFun() {
-        final int initialFun = this.fun.getValueStat();
-
-        this.gameRoomLogic.play(this.fun);
-
-        assertEquals(initialFun + GameRoomLogic.INCREMENT_ACTION_PLAY, this.fun.getValueStat(),
-                "Giocare deve aumentare il divertimento");
-    }
-
-    @Test
-    void testPlayClamping() {
-        this.fun.setValueStat(SET_FUN_VALUE);
-
-        this.gameRoomLogic.play(this.fun);
-
-        assertEquals(PouStatistics.STATISTIC_MAX_VALUE, this.fun.getValueStat(),
-                "L'azione play non deve far aumentare la statistica sopra al limite");
+    void testPlay() {
+        final int expectedIncrement = 15;
+        gameRoomLogic.play(fun);
+        assertEquals(INITIAL_VALUE + expectedIncrement, fun.getValueStat());
     }
 }
