03f53c578666d33915ca9348899c4d0bdd148580
Rei Mici <rei.mici2@studio.unibo.it>
2026-02-05 12:25:06 +0100

    Update KitchenLogic to accept Food items

--- src/main/java/it/unibo/jpou/mvc/model/roomlogic/KitchenLogic.java
+++ src/main/java/it/unibo/jpou/mvc/model/roomlogic/KitchenLogic.java
@@ -1,20 +1,22 @@
 package it.unibo.jpou.mvc.model.roomlogic;
 
 import it.unibo.jpou.mvc.model.PouStatistics;
+import it.unibo.jpou.mvc.model.items.consumable.food.Food;
 
 /**
  * Logic for Kitchen, action eat.
  */
 public final class KitchenLogic {
 
-    public static final int INCREMENT_ACTION_EAT = 10;
-
     /**
-     * Meke Pou eat.
+     * Make Pou eat a specific food.
      *
-     * @param hunger the statistic to modify
+     * @param hunger the hunger statistic to modify.
+     * @param food the food item being consumed.
      */
-    public void eat(final PouStatistics hunger) {
-        hunger.setValueStat(hunger.getValueStat() + INCREMENT_ACTION_EAT);
+    public void eat(final PouStatistics hunger, final Food food) {
+        if (food != null) {
+            hunger.setValueStat(hunger.getValueStat() + food.getEffectValue());
+        }
     }
 }
