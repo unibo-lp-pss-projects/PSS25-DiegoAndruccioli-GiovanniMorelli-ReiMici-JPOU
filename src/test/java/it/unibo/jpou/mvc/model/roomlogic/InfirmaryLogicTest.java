0421ef4126aa8563a1311914cfc59289d66cf4ce
Rei Mici <rei.mici2@studio.unibo.it>
2026-02-05 12:25:15 +0100

    Update InfirmaryLogicTest for Potion usage and fix magic numbers

--- src/test/java/it/unibo/jpou/mvc/model/roomlogic/InfirmaryLogicTest.java
+++ src/test/java/it/unibo/jpou/mvc/model/roomlogic/InfirmaryLogicTest.java
@@ -1,48 +1,45 @@
 package it.unibo.jpou.mvc.model.roomlogic;
 
-import it.unibo.jpou.mvc.model.PouStatistics;
+import it.unibo.jpou.mvc.model.items.consumable.potion.EnergyPotion;
+import it.unibo.jpou.mvc.model.items.consumable.potion.HealthPotion;
 import it.unibo.jpou.mvc.model.statistics.EnergyStatistic;
 import it.unibo.jpou.mvc.model.statistics.HealthStatistic;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class InfirmaryLogicTest {
 
-    private PouStatistics energy;
-    private PouStatistics health;
+    private static final int INITIAL_VALUE = 50;
     private InfirmaryLogic infirmaryLogic;
+    private EnergyStatistic energy;
+    private HealthStatistic health;
 
     @BeforeEach
     void setUp() {
-        this.energy = new EnergyStatistic();
-        this.health = new HealthStatistic();
-        this.infirmaryLogic = new InfirmaryLogic();
-    }
-
-    @Test
-    void testUseEnergyPotion() {
-        final int initialEnergy = this.energy.getValueStat();
-
-        this.infirmaryLogic.usePotion("EnergyPotion", this.energy, this.health);
-
-        assertAll("La pozione aumenta solo la statistica dell'energia",
-                () -> assertEquals(initialEnergy + InfirmaryLogic.INCREMENT_ENERGY_POTION,
-                        this.energy.getValueStat()),
-                () -> assertEquals(PouStatistics.STATISTIC_INITIAL_VALUE, this.health.getValueStat()));
+        infirmaryLogic = new InfirmaryLogic();
+        energy = new EnergyStatistic();
+        health = new HealthStatistic();
+        energy.setValueStat(INITIAL_VALUE);
+        health.setValueStat(INITIAL_VALUE);
     }
 
     @Test
     void testUseHealthPotion() {
-        final int initialHealth = this.health.getValueStat();
+        final HealthPotion potion = new HealthPotion();
+        final int expectedHealth = INITIAL_VALUE + potion.getEffectValue();
+        infirmaryLogic.usePotion(energy, health, potion);
+        assertEquals(expectedHealth, health.getValueStat());
+        assertEquals(INITIAL_VALUE, energy.getValueStat());
+    }
 
-        this.infirmaryLogic.usePotion("HealthPotion", this.energy, this.health);
-
-        assertAll("La pozione aumenta solo la statistica della salute",
-                () -> assertEquals(initialHealth + InfirmaryLogic.INCREMENT_HEALTH_POTION,
-                        this.health.getValueStat()),
-                () -> assertEquals(PouStatistics.STATISTIC_INITIAL_VALUE, this.energy.getValueStat()));
+    @Test
+    void testUseEnergyPotion() {
+        final EnergyPotion potion = new EnergyPotion();
+        final int expectedEnergy = INITIAL_VALUE + potion.getEffectValue();
+        infirmaryLogic.usePotion(energy, health, potion);
+        assertEquals(expectedEnergy, energy.getValueStat());
+        assertEquals(INITIAL_VALUE, health.getValueStat());
     }
 }
