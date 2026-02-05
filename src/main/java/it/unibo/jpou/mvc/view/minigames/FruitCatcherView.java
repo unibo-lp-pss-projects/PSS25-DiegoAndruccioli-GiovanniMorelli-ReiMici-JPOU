0db770ab67e620d867e9d591e93169237b6908ff
Giovanni Morelli <giovanni.morelli8@studio.unibo.it>
2026-02-05 12:42:11 +0100

    refactoring fcg view

--- src/main/java/it/unibo/jpou/mvc/view/minigames/FruitCatcherView.java
+++ src/main/java/it/unibo/jpou/mvc/view/minigames/FruitCatcherView.java
@@ -1,21 +1,29 @@
 package it.unibo.jpou.mvc.view.minigames;
 
 import it.unibo.jpou.mvc.model.minigames.fruitcatcher.FallingObject;
+import javafx.scene.Parent;
 import java.util.List;
 
 /**
  * Interface for the visual representation of the Fruit Catcher minigame.
  */
-@FunctionalInterface
 public interface FruitCatcherView {
 
     /**
      * Renders the current state of the game.
      *
-     * @param objects   the list of falling objects to draw.
-     * @param score     the current score.
+     * @param objects   the list of falling objects (apples, bombs) to draw.
+     * @param score     the current score to display.
      * @param gameOver  true if the game is over.
      * @param playerX   the current X position of the player.
      */
     void render(List<FallingObject> objects, int score, boolean gameOver, double playerX);
+
+    /**
+     * Gets the JavaFX root node of this view.
+     * Used by the MainController to switch the scene to this game.
+     *
+     * @return the root Parent node.
+     */
+    Parent getNode();
 }
