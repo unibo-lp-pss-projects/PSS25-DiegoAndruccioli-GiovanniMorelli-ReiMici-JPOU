f6b89c62065435bf035aa9c9465b6bfb1b6a7838
Rei Mici <rei.mici2@studio.unibo.it>
2026-02-05 12:25:15 +0100

    Update KitchenLogicTest for Food usage and fix magic numbers

--- src/test/java/it/unibo/jpou/mvc/model/roomlogic/KitchenLogicTest.java
+++ src/test/java/it/unibo/jpou/mvc/model/roomlogic/KitchenLogicTest.java
@@ -1,6 +1,7 @@
 package it.unibo.jpou.mvc.model.roomlogic;
 
-import it.unibo.jpou.mvc.model.PouStatistics;
+import it.unibo.jpou.mvc.model.items.consumable.food.Apple;
+import it.unibo.jpou.mvc.model.items.consumable.food.Food;
 import it.unibo.jpou.mvc.model.statistics.HungerStatistic;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -9,22 +10,28 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class KitchenLogicTest {
 
-    private PouStatistics hunger;
+    private static final int INITIAL_VALUE = 50;
     private KitchenLogic kitchenLogic;
+    private HungerStatistic hunger;
 
     @BeforeEach
     void setUp() {
-        this.hunger = new HungerStatistic();
-        this.kitchenLogic = new KitchenLogic();
+        kitchenLogic = new KitchenLogic();
+        hunger = new HungerStatistic();
+        hunger.setValueStat(INITIAL_VALUE);
     }
 
     @Test
-    void testIncreasesHunger() {
-        final int initialHunger = this.hunger.getValueStat();
-        this.kitchenLogic.eat(this.hunger);
-
-        assertEquals(initialHunger + KitchenLogic.INCREMENT_ACTION_EAT, this.hunger.getValueStat(),
-                "Mangiare deve aumentare la fame");
+    void testEat() {
+        final Food apple = new Apple();
+        final int expectedValue = INITIAL_VALUE + apple.getEffectValue();
+        kitchenLogic.eat(hunger, apple);
+        assertEquals(expectedValue, hunger.getValueStat());
     }
 
+    @Test
+    void testEatNull() {
+        kitchenLogic.eat(hunger, null);
+        assertEquals(INITIAL_VALUE, hunger.getValueStat());
+    }
 }
